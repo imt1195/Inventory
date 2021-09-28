93f18fffd867b70de82950e9d2dfee424e6b2171
Imt1195 <imt1195@gmail.com>
2021-09-28 10:43:40 -0400

    seeding data added

--- server/knex/seeds/seed_storage.js
+++ server/knex/seeds/seed_storage.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, name: 'WD Blue', manufacturer: 'Western Digital', form_factor: '5.35 inch', capacity_gb: 1028, interface: 'SATA', quantity: 1},
+        {id: 2, name: 'WD Black', manufacturer: 'Western Digital', form_factor: '5.35 inch', capacity_gb: 1028, interface: 'SATA', quantity: 1},
+        {id: 3, name: 'Seagate BarraCuda', manufacturer: 'Seagate', form_factor: '5.35 inch', capacity_gb: 2056, interface: 'SATA', quantity: 1}
       ]);
     });
 };
--- server/knex/seeds/seed_tools.js
+++ server/knex/seeds/seed_tools.js
@@ -5,9 +5,10 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+        {id: 1, name: 'Soldering Iron', description: 'Hakko 850W Soldering iron', status: 'Available'},
+        {id: 2, name: 'Digital Caliper', description: 'Digital Caliper', status: 'Available'},
+        {id: 3, name: 'Multimeter', description: 'Handheld Multimeter', status: 'Broken/Needs Maintenance'},
+        {id: 3, name: 'Ender 3 Pro', description: 'Ender 3 Pro 3D printer', status: 'Available'}
       ]);
     });
 };
